c8489138758baacb368fb60f08356bd057c23e74
Griiid <gridwing@gmail.com>
2022-09-13 12:38:40 +0800

    Detect lolcat installed or not

--- random-ascii.sh
+++ random-ascii.sh
@@ -3,5 +3,10 @@
 R=$[$RANDOM % 132]
 PICTURE=$(cat "$HOME/Open Source/ascii-art-collection/arts/$R.txt")
 echo
-echo "$PICTURE" | lolcat
+if ! command -v lolcat  &> /dev/null
+then
+  echo "$PICTURE"
+else
+  echo "$PICTURE" | lolcat
+fi
 echo -ne "\n\n"
